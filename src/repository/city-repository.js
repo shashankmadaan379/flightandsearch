a420e93b1e0aad82ec1561a4f2ebce3ef2d0dbae
shashankmadaan379 <codebreaker379@gmail.com>
2023-04-26 22:29:31 +0530

    updateCity and getCity fucntions

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,5 +1,6 @@
 const e = require("express");
 const { City } = require("../models/index");
+const city = require("../models/city");
 
 class CityRepository {
   //create a city
@@ -8,7 +9,8 @@ class CityRepository {
       const city = await City.create({ name });
       return city;
     } catch (error) {
-      console.log(error);
+      console.log("Something went wrong in repository layer");
+      throw error;
     }
   }
 
@@ -20,8 +22,33 @@ class CityRepository {
           id: cityID,
         },
       });
+      return true;
+    } catch (error) {
+      console.log("Something went wrong in repository layer");
+      throw error;
+    }
+  }
+
+  async updateCity(cityID, data) {
+    try {
+      const city = await city.update(data, {
+        where: {
+          id: cityID,
+        },
+      });
+      return city;
     } catch (error) {
       console.log(error);
+    }
+  }
+
+  async getCity(cityID) {
+    try {
+      const city = await City.findByPk(cityID);
+      return city;
+    } catch (error) {
+      console.log("Something went wrong in repository layer");
+      throw error;
     }
   }
 }
