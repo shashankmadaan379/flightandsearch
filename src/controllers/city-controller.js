5f59af45fbc8656db02ebc97e8472787d32d5df7
shashankmadaan379 <codebreaker379@gmail.com>
2023-04-27 01:23:10 +0530

    added createController

---
+++ src/controllers/city-controller.js
@@ -0,0 +1,32 @@
+const { CityService } = require("../services/index");
+
+const CityService = new CityService();
+const create = (req, res) => {
+  try {
+    const city = CityService.createCity(req.body);
+    return res.status(201).json({
+      data: city,
+      success: true,
+      message: "Succesfully created city !",
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: "Not able to create a city",
+      err: error,
+    });
+  }
+};
+const destroy = (req, res) => {};
+const update = (req, res) => {};
+const get = (req, res) => {};
+
+module.exports = {
+  create,
+  destroy,
+  update,
+  get,
+};
