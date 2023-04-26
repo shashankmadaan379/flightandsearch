87ece5460e2948cc6d7e1d602038683ec923b963
shashankmadaan379 <codebreaker379@gmail.com>
2023-04-27 01:35:02 +0530

    added get controller

--- src/controllers/city-controller.js
+++ src/controllers/city-controller.js
@@ -35,13 +35,31 @@ const destroy = async (req, res) => {
     return res.status(500).json({
       data: {},
       success: false,
-      message: "Not able to delete a city",
+      message: "Not able to delete the city",
       err: error,
     });
   }
 };
 const update = (req, res) => {};
-const get = (req, res) => {};
+const get = async (req, res) => {
+  try {
+    const city = await cityService.getCity(req.params.id);
+    return res.status(201).json({
+      data: city,
+      success: true,
+      message: "Succesfully get the city !",
+      arr: {},
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: "Not able to get the city",
+      err: error,
+    });
+  }
+};
 
 module.exports = {
   create,
