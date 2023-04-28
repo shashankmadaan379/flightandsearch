d0cb75e080dc556dc4f6428e12ca99937fed6014
shashankmadaan379 <codebreaker379@gmail.com>
2023-04-28 14:01:45 +0530

    associate and migrate city and airport models

--- src/models/airport.js
+++ src/models/airport.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Airport extends Model {
     /**
@@ -11,15 +9,22 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.belongsTo(models.City, {
+        foreignKey: cityId,
+        onDelete: "CASCADE",
+      });
     }
   }
-  Airport.init({
-    name: DataTypes.STRING,
-    address: DataTypes.STRING,
-    cityId: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Airport',
-  });
+  Airport.init(
+    {
+      name: { type: DataTypes.STRING, allowNull: false },
+      address: DataTypes.STRING,
+      cityId: { type: DataTypes.INTEGER, allowNull: false },
+    },
+    {
+      sequelize,
+      modelName: "Airport",
+    }
+  );
   return Airport;
 };
