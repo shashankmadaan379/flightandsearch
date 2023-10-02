b75e0bf2c4a52e829860e5a23eb286db27080cc0
Shashank <shashankmadaan379@gmail.com>
2023-10-02 12:08:05 +0530

    Changes in compareTime function

--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -1,5 +1,5 @@
 const { FlightRepository, AirplaneRepository } = require("../repository/index");
-const { compareTime } = require("../utils/helper");
+const compareTime = require("../utils/helper");
 class FlightService {
   constructor() {
     this.FlightRepository = new FlightRepository();
@@ -8,7 +8,7 @@ class FlightService {
 
   async createFlight(data) {
     try {
-      if (!compareTime(data.arrivalTime > data.departureTime)) {
+      if (!compareTime(data.arrivalTime,data.departureTime)) {
         throw { error: "Arrival time cannot be less than departure time" };
       }
       const airplane = await this.AirplaneRepository.getAirplane(
